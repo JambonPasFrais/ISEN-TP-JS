d26db4b8b43dc1f3476d09efcfbb5bf4f61b6379
JambonPasFrais <60687226+JambonPasFrais@users.noreply.github.com>
2021-03-17 08:17:49 +0100

    Update on exo6

--- TP1/exo6.js
+++ TP1/exo6.js
@@ -1,21 +1,16 @@
-var Histogramme = function (myArray) {
-    var myTab1 = new Array(Math.max.apply(null, myArray));
-    for (var i = 0; i < 11; ++i){
-        myTab1[i] = new Array(myArray.length);
+let Histogramme = function (myArray) {
+    let myTab1 = new Array(myArray.length);
+    //Create a 2D Tab
+    for (let i = 0; i < myTab1.length; ++i){
+        myTab1[i] = new Array(Math.max(...myArray));
     }
-    for (var j = 0; j < myArray.length; j++){
-        for (var i = 0; i < myTab1.length - myArray[j]; i++){
-            myTab1[i][j] = '.';
-        }
-    }
-    for (var i = 0; i < myTab1.length; i++){
-        for (var j = 0; j < myArray.length; j++){
-            if (myTab1[i][j] != '.'){
-                myTab1[i][j] = '#';
-            }
+    
+    for (let i = 0; i < myTab1.length; i++){
+        for (let j = 0; j < myArray[i]; j++){
+            myTab1[i][j] = '#';                
         }
     }
     console.table(myTab1);
 }
-var myArray = [4, 1, 8, 3, 10, 6, 7, 2, 9, 3];
+let myArray = [4, 1, 8, 3, 10, 6, 7, 2, 9, 3, 12, 10, 3];
 Histogramme(myArray);
